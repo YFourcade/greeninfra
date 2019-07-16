af11ce42760d56934875aeff0d72f4304e0fb1f1
YFourcade <yoanfourcade@gmail.com>
2019-07-16 21:41:01 +0200

    Updated code to run NMDS (including plots) for all taxa

--- script.R
+++ script.R
@@ -6,92 +6,118 @@ library(betapart)
 
 # sites
 sites <- rio::import("../site data.xlsx")
+sites$Landscape <- as.numeric(gsub(4711, 7411, sites$Landscape)) # fix ?
 
 # surveys
 data_BB <- rio::import("../Bumblebees_GINFRA_allfix.xlsx") %>%
   select(-contains("sex"), - contains("pollen"), -Observer, -Honeybee, -Notes, - Others) %>% as.tbl %>%
   replace(., is.na(.), 0) %>% 
   dplyr::select(1,2,10:29) %>%
-  group_by(Landscape, Transect_type) %>% summarise_all(sum)
+  group_by(Landscape, Transect_type) %>% summarise_all(sum) %>%
+  gather(-1, -2, key = "Species", value = "n")
 
+data_But <- rio::import("../Butterflies_GINFRA_allfix.xlsx") %>%
+  select(-Observer, -Note) %>% as.tbl %>%
+  replace(., is.na(.), 0) %>% 
+  dplyr::select(1,2,10:73) %>%
+  group_by(Landscape, Transect_type) %>% summarise_all(sum) %>%
+  gather(-1, -2, key = "Species", value = "n")
 
+data_Plants <- rio::import("../PlantsPlots_GINFRA.xlsx") %>%
+  select(-Observer) %>% as.tbl %>% mutate(value = 1) %>%
+  spread(key = "Species", value = "value", fill = 0) %>% 
+  dplyr::select(1,2,5:138) %>%
+  rename(Transect_type = `Transect Type`) %>%
+  group_by(Landscape, Transect_type) %>% summarise_all(sum) %>%
+  gather(-1, -2, key = "Species", value = "n")
 
-data <- data_BB %>% left_join(sites) 
+data_Plants$Landscape <- as.numeric(gsub(5560, 5561, data_Plants$Landscape)) # fix ?
+data_Plants$Transect_type <- gsub("Between fields (between meadows)", "Between fields", data_Plants$Transect_type, fixed=TRUE)
+data_Plants$Transect_type <- gsub("Small road (eastern transect)", "Small road", data_Plants$Transect_type, fixed=TRUE)
+
+data <- bind_rows("Bumblebees" = data_BB, "Butterflies" = data_But, "Plants" = data_Plants, .id = "taxon") %>% left_join(sites) 
 
 ### test effect of habitat types ###
 
 ### NMDS ###
-data.perm <- data %>% ungroup %>% filter(rowSums(.[,-c(1:2, 23:29)]) > 0)
+ndms.plots <- c()
+for(i in unique(data$taxon)){
+    data.nmds <- data %>% filter(taxon == i) %>% ungroup %>% 
+      dplyr::select(2:8) %>%
+      spread(key = Species, value = n) %>%
+      filter(rowSums(.[,-c(1:5)]) > 0)
+    
+    nmds <- metaMDS(data.nmds[,-c(1:5)], trymax = 10000, maxit = 20000)
+    
+    # plot #
+    # prepare scores
+    nmds.scores <- as.data.frame(scores(nmds))
+    nmds.scores$site <- rownames(nmds.scores)
+    nmds.scores$road <- data.nmds$`Road Density`
+    nmds.scores$power <- data.nmds$Powerline
+    nmds.scores$habitat <- factor(data.nmds$Transect_type, levels = c("Powerline",
+                                                                      "Between fields",
+                                                                      "Pasture",
+                                                                      "Small road",
+                                                                      "Big road"))
+    
+    
+    # species.scores <- as.data.frame(scores(nmds, "species"))
+    # species.scores$species <- rownames(species.scores)
+    
+    # create hull polygons
+    
+    # habitat
+    habitat.1 <- nmds.scores[nmds.scores$habitat == 
+                               "Between fields", ][chull(nmds.scores[nmds.scores$habitat == 
+                                                                       "Between fields", c("NMDS1", "NMDS2")]), ] 
+    habitat.2 <- nmds.scores[nmds.scores$habitat == 
+                               "Big road", ][chull(nmds.scores[nmds.scores$habitat == 
+                                                                 "Big road", c("NMDS1", "NMDS2")]), ]
+    habitat.3 <- nmds.scores[nmds.scores$habitat == 
+                               "Pasture", ][chull(nmds.scores[nmds.scores$habitat == 
+                                                                "Pasture", c("NMDS1", "NMDS2")]), ] 
+    habitat.4 <- nmds.scores[nmds.scores$habitat == 
+                               "Powerline", ][chull(nmds.scores[nmds.scores$habitat == 
+                                                                  "Powerline", c("NMDS1", "NMDS2")]), ] 
+    habitat.5 <- nmds.scores[nmds.scores$habitat == 
+                               "Small road", ][chull(nmds.scores[nmds.scores$habitat == 
+                                                                   "Small road", c("NMDS1", "NMDS2")]), ] 
+    hull.habitat <- rbind(habitat.1, habitat.2) 
+    hull.habitat <- rbind(hull.habitat, habitat.3) 
+    hull.habitat <- rbind(hull.habitat, habitat.4) 
+    hull.habitat <- rbind(hull.habitat, habitat.5) 
+    
+    hull.habitat$habitat <- factor(hull.habitat$habitat, levels = c("Powerline",
+                                                                    "Between fields",
+                                                                    "Pasture",
+                                                                    "Small road",
+                                                                    "Big road"))
+    
+    
+    ndms.plots <- rbind.data.frame(ndms.plots, 
+                                   cbind.data.frame(taxon = i,
+                                   rbind.data.frame(cbind.data.frame(what = "hull", hull.habitat),
+                                                    cbind.data.frame(what = "scores", nmds.scores)
+                                                    )
+                                   )
+    )
+}
 
-nmds <- metaMDS(data.perm[,-c(1:2, 23:29)], trymax = 10000, maxit = 20000)
-
-# plot #
-# prepare scores
-nmds.scores <- as.data.frame(scores(nmds))
-nmds.scores$site <- rownames(nmds.scores)
-nmds.scores$road <- data.perm$`Road Density`
-nmds.scores$power <- data.perm$Powerline
-nmds.scores$habitat <- factor(data.perm$Transect_type, levels = c("Powerline",
-                                                                  "Between fields",
-                                                                  "Pasture",
-                                                                  "Small road",
-                                                                  "Big road"))
-
-
-species.scores <- as.data.frame(scores(nmds, "species"))
-species.scores$species <- rownames(species.scores)
-
-# create hull polygons
-# roads
-road.low <- nmds.scores[nmds.scores$road == "low", ][chull(nmds.scores[nmds.scores$road == "low", c("NMDS1", "NMDS2")]), ] 
-road.high <- nmds.scores[nmds.scores$road == "high", ][chull(nmds.scores[nmds.scores$road == "high", c("NMDS1", "NMDS2")]), ]
-hull.road <- rbind(road.low, road.high) 
-
-# powerlines
-power.yes <- nmds.scores[nmds.scores$power == "Yes", ][chull(nmds.scores[nmds.scores$power == "Yes", c("NMDS1", "NMDS2")]), ] 
-power.no <- nmds.scores[nmds.scores$power == "No", ][chull(nmds.scores[nmds.scores$power == "No", c("NMDS1", "NMDS2")]), ]
-hull.power <- rbind(power.yes, power.no) 
-
-# habitat
-habitat.1 <- nmds.scores[nmds.scores$habitat == 
-                           "Between fields", ][chull(nmds.scores[nmds.scores$habitat == 
-                                                                   "Between fields", c("NMDS1", "NMDS2")]), ] 
-habitat.2 <- nmds.scores[nmds.scores$habitat == 
-                           "Big road", ][chull(nmds.scores[nmds.scores$habitat == 
-                                                             "Big road", c("NMDS1", "NMDS2")]), ]
-habitat.3 <- nmds.scores[nmds.scores$habitat == 
-                           "Pasture", ][chull(nmds.scores[nmds.scores$habitat == 
-                                                            "Pasture", c("NMDS1", "NMDS2")]), ] 
-habitat.4 <- nmds.scores[nmds.scores$habitat == 
-                           "Powerline", ][chull(nmds.scores[nmds.scores$habitat == 
-                                                              "Powerline", c("NMDS1", "NMDS2")]), ] 
-habitat.5 <- nmds.scores[nmds.scores$habitat == 
-                           "Small road", ][chull(nmds.scores[nmds.scores$habitat == 
-                                                               "Small road", c("NMDS1", "NMDS2")]), ] 
-hull.habitat <- rbind(habitat.1, habitat.2) 
-hull.habitat <- rbind(hull.habitat, habitat.3) 
-hull.habitat <- rbind(hull.habitat, habitat.4) 
-hull.habitat <- rbind(hull.habitat, habitat.5) 
-
-hull.habitat$habitat <- factor(hull.habitat$habitat, levels = c("Powerline",
-                                                                  "Between fields",
-                                                                  "Pasture",
-                                                                  "Small road",
-                                                                  "Big road"))
-
-
-# plots
 ggplot() + 
-  geom_polygon(data=hull.habitat,aes(x=NMDS1,y=NMDS2,fill=habitat ,group=habitat ),alpha=0.25) + # add the convex hulls
-  geom_point(data=nmds.scores,aes(x=NMDS1,y=NMDS2,shape=habitat,colour=habitat),size=3) + # add the point markers
-  geom_text(data=species.scores,aes(x=NMDS1,y=NMDS2,label=species),alpha=0.5, size = 3.5) +  # add the species labels
+  geom_polygon(data=ndms.plots[ndms.plots$what == "hull",],
+               aes(x=NMDS1,y=NMDS2,fill=habitat ,group=habitat ),alpha=0.25) + # add the convex hulls
+  # geom_text(data=species.scores,aes(x=NMDS1,y=NMDS2,label=species),alpha=0.5, size = 3.5) +  # add the species labels
+  geom_point(data=ndms.plots[ndms.plots$what == "scores",],
+             aes(x=NMDS1,y=NMDS2,shape=habitat,colour=habitat),size=3) + # add the point markers
+  facet_grid(~ taxon) +
   coord_equal() +
   scale_color_discrete("Habitat type") +
   scale_fill_discrete("Habitat type") +
   scale_shape_discrete("Habitat type") +
   theme_bw()
 
-ggsave("nmds_plot.svg", width = 6, height = 4.5, dpi = 600)
+ggsave("nmds_plot.svg", width = 12, height = 5)
 
 ### beta-diversity ###
 
@@ -124,3 +150,15 @@ data %>% group_by(Transect_type) %>%
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 ggsave("beta.div_plot.svg", width = 8, height = 4)
+
+
+
+#######################
+## clean environment ##
+#######################
+gdata::keep(data, 
+            ndms.plots,
+            sure = T)
+
+
+
